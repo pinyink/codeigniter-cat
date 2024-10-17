a3725890ab1ac3b4bc16a747e35ea66cf373011f
pinyink <nurkhafindi@gmail.com>
2024-10-17 16:03:31 +0700

    delete

--- app/Views/product_categories/_detail.php
+++
@@ -1,7 +0,0 @@
-<table class="table">
-	<tr>
-		<td style="width: 25%;">Nama Kategories</td>
-		<td style="width: 1%;">:</td>
-		<th style="width: 75%;"><?=esc($product_categories['nama']); ?></th>
-	</tr>
-</table>
